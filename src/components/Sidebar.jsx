18a949484aca869e6c1ccfe1b3d98ce378608732
Saikat Mandal <msaikat022@gmail.com>
2025-02-01 09:17:54 +0530

    Structure modified to basic layout

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Sidebar = () => {
+    return (
+        <div className='min-h-screen bg-black md:w-2/12 w-0'>
+            <h1 className='text-white'>Sidebar</h1>
+        </div>
+    )
+}
+
+export default Sidebar
---
+++ src/pages/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import PublicLayout from '../layouts/PublicLayout'
+import Notes from '../components/Notes'
+import Create from '../components/Create'
+
+const Home = () => {
+    return (
+        <PublicLayout>
+            <div className='flex'>
+                <Notes />
+                <Create />
+            </div>
+
+        </PublicLayout>
+    )
+}
+
+export default Home
